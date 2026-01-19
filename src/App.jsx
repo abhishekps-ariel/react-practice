3a9461884a8f57f9b93ed2a2ff6f4e6082a40065
abhishek <abhishekp@arielsoftwares.in>
2026-01-19 12:56:05 +0530

    design changes to the color controller

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ function App() {
   
   return (
     <>
-    <div className=' bg-white h-screen w-screen flex justify-center' style={{backgroundColor:bgColor}}>
-      <div className=' mt-130 absolute flex flex-wrap gap-10'> 
-        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('blue')}>Blue</button>
-        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('red')}>Red</button>
-        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('green')}>Green</button>
-        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('White')}>Clear</button>
+    <div className=' bg-white h-screen w-screen flex justify-center duration-200' style={{backgroundColor:bgColor}}>
+      <div className=' mt-130 absolute flex flex-wrap gap-10'>
+        <div className='flex flex-wrap gap-10 bg-white px-5 py-3 rounded-3xl shadow-2xl border'>
+        <button className=' bg-white border shadow-lg font-bold px-3 py-2 cursor-pointer rounded-3xl' onClick={()=>setBgColor('red')}>Red</button>
+        <button className=' bg-white border shadow-lg font-bold px-3 py-2 cursor-pointer rounded-3xl' onClick={()=>setBgColor('blue')}>Blue</button>
+        <button className=' bg-white border shadow-lg font-bold px-3 py-2 cursor-pointer rounded-3xl' onClick={()=>setBgColor('green')}>Green</button>
+        <button className=' bg-white border shadow-lg font-bold px-3 py-2 cursor-pointer rounded-3xl' onClick={()=>setBgColor('White')}>Clear</button>
+        </div> 
       </div>
     </div>
     </>
