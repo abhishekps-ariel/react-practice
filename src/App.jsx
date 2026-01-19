d7f3eddcdf040db2a141414b389fac63d7c69b10
abhishek <abhishekp@arielsoftwares.in>
2026-01-19 12:35:46 +0530

    background color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [bgColor, setBgColor]=useState('white')
+  
   return (
     <>
-    <div>React Practice</div>
+    <div className=' bg-white h-screen w-screen flex justify-center' style={{backgroundColor:bgColor}}>
+      <div className=' mt-130 absolute flex flex-wrap gap-10'> 
+        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('blue')}>Blue</button>
+        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('red')}>Red</button>
+        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('green')}>Green</button>
+        <button className=' bg-gray-200 border shadow-2xl px-3 py-2 cursor-pointer' onClick={()=>setBgColor('White')}>Clear</button>
+      </div>
+    </div>
     </>
   )
 }
